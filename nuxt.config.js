4328d9254089ab3883158410489331afe1324118
Philip Clarke <send2philip@gmail.com>
2021-01-23 06:06:01 +0000

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,67 @@
+export default {
+  /*
+   ** Headers of the page
+   ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
+   */
+
+  head: {
+    title: "Philip's Site",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "descriptive text here"
+      }
+    ],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Montserrat:300,700"
+      }
+    ]
+  },
+
+  /*
+   ** Global CSS
+   ** Doc: https://nuxtjs.org/api/configuration-css
+   */
+  css: [],
+
+  /*
+   ** Plugins to load before mounting the App
+   ** Doc: https://nuxtjs.org/guide/plugins
+   */
+  plugins: [],
+
+  /*
+   ** Nuxt.js modules
+   ** Doc: https://nuxtjs.org/guide/modules
+   */
+  modules: [
+    // Doc: https://http.nuxtjs.org
+    "@nuxt/http",
+    // TODO: Remove it if you want to eject from codeSandbox
+    "./codesandbox"
+  ],
+
+  /*
+   ** HTTP module configuration
+   */
+  http: {
+    // See https://http.nuxtjs.org/api/#options
+  },
+
+  /*
+   ** Build configuration
+   ** Doc: https://nuxtjs.org/api/configuration-build
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+};
